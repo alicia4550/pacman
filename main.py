422308a6e4fbafe6fc3434d9b9ecfa58196b0362
Alicia Tran <aliciatran4550@gmail.com>
2023-10-14 22:37:52 -0400

    feat: add dots and pellets

--- main.py
+++ main.py
@@ -41,6 +41,8 @@ class Player(pygame.sprite.Sprite):
         self.movey = 0
         self.frame = 0
 
+        self.score = 0
+
     def move(self, key):
         if key[pygame.K_LEFT]:
             rect = pygame.Rect(self.rect.x - 1, self.rect.y, self.rect.width, self.rect.height)
@@ -77,6 +79,18 @@ class Player(pygame.sprite.Sprite):
         else:
             self.stop()
 
+    def eat(self):
+        if self.rect.collidelist(dots) is not -1:
+            index = self.rect.collidelist(dots)
+            dots.pop(index)
+            self.score += 10
+            print(self.score)
+        if self.rect.collidelist(pellets) is not -1:
+            index = self.rect.collidelist(pellets)
+            pellets.pop(index)
+            self.score += 50
+            print(self.score)
+
     def control(self, x, y):
         self.movex = x
         self.movey = y
@@ -98,6 +112,22 @@ class Wall(object):
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
     def draw(self, win):
         pygame.draw.rect(win, (0, 255, 0), self.rect, 2)
+
+class Dot(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.rect = pygame.Rect(self.x, self.y, 4.5, 4.5)
+    def draw(self, win):
+        pygame.draw.rect(win, (255, 183, 174), self.rect, 2)
+
+class Pellet(object):
+    def __init__(self, x, y):
+        self.x = (x * 32)
+        self.y = (y * 32)
+        self.rect = pygame.Rect(self.x, self.y, 16, 16)
+    def draw(self, win):
+        pygame.draw.circle(win, (255, 183, 174), (self.x, self.y), 8)
 
 '''
     FUNCTIONS
@@ -127,12 +157,12 @@ clock = pygame.time.Clock()
 
 # Create background
 screen.fill((50, 50, 50))
-map = get_image(spritesheet, 0, 0, SPRITE_PIXEL_SIZE*GRID_SPRITE_WIDTH, SPRITE_PIXEL_SIZE*GRID_SPRITE_HEIGHT, False)
+map = get_image(spritesheet, 1.02, 0, SPRITE_PIXEL_SIZE*GRID_SPRITE_WIDTH, SPRITE_PIXEL_SIZE*GRID_SPRITE_HEIGHT, False)
 
 # Create Player
 player = Player()
-player.rect.x = (GRID_SPRITE_WIDTH - 0.5) * SPRITE_PIXEL_SIZE
-player.rect.y = (GRID_SPRITE_HEIGHT + 0.5) * SPRITE_PIXEL_SIZE
+player.rect.x = (GRID_SPRITE_WIDTH - 0.75) * SPRITE_PIXEL_SIZE
+player.rect.y = (GRID_SPRITE_HEIGHT + 6.5) * SPRITE_PIXEL_SIZE
 player_list = pygame.sprite.Group()
 player_list.add(player)
 steps = 1
@@ -231,6 +261,47 @@ walls = [
     wall42
 ]
 
+noSpawn1 = Wall(3.5, 4.75, 7, 5)
+noSpawn2 = Wall(0, 4.75, 2.75, 5)
+noSpawn3 = Wall(11.25, 4.75, 2.75, 5)
+
+noSpawn4 = Wall(6.5, 11.25, 1, 1)
+
+noSpawn5 = Wall(0.5, 1.5, 0.5, 0.5)
+noSpawn6 = Wall(13, 1.5, 0.5, 0.5)
+noSpawn7 = Wall(0.5, 11.5, 0.5, 0.5)
+noSpawn8 = Wall(13, 11.5, 0.5, 0.5)
+
+noSpawns = [
+    noSpawn1,
+    noSpawn2,
+    noSpawn3,
+    noSpawn4,
+    noSpawn5,
+    noSpawn6,
+    noSpawn7,
+    noSpawn8
+]
+
+dots = []
+for i in range(22, 438, 16):
+    for j in range(22, 486, 16):
+        dot = Dot(i, j)
+        if dot.rect.collidelist(walls) is -1 and dot.rect.collidelist(noSpawns) is -1:
+            dots.append(dot)
+
+pellet1 = Pellet(0.75, 1.75)
+pellet2 = Pellet(0.75, 11.75)
+pellet3 = Pellet(13.25, 1.75)
+pellet4 = Pellet(13.25, 11.75)
+
+pellets = [
+    pellet1,
+    pellet2,
+    pellet3,
+    pellet4
+]
+
 animStartTime = time.time()
 
 '''
@@ -246,6 +317,7 @@ while running:
     key = pygame.key.get_pressed()
     
     player.move(key)
+    player.eat()
 
     screen.blit(map, (0, 0))
     if time.time() - animStartTime > 0.5:
@@ -255,10 +327,14 @@ while running:
             player.image = player.images[0]
         animStartTime = time.time()
     player.update()
-    player_list.draw(screen)
 
-    # for wall in walls:
-    #     wall.draw(screen)
+    for dot in dots:
+        dot.draw(screen)
+
+    for pellet in pellets:
+        pellet.draw(screen)
+
+    player_list.draw(screen)
 
     pygame.display.flip()
     pygame.display.update()
@@ -267,4 +343,3 @@ while running:
 
 # Done! Time to quit.
 pygame.quit()
-
